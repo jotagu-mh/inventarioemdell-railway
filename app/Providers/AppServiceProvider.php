cf45c22a49d3af67503bf3dc0e804adb1c1203f8
Juan Mollericona <jmollericona18@gmail.com>
2026-02-03 20:34:00 -0400

    Seeder admin con variable de entorno listo para Railway

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,5 +15,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
+
+        Artisan::call('db:seed', [
+            '--class' => 'UserSeeder',
+            '--force' => true
+        ]);
     }
 }
